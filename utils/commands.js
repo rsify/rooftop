ab58742f29c9bb0bfc0a23c6a5dd62b0f2fa7d59
Maciej Łaszcz <nikersifyyt@gmail.com>
2016-08-27 12:12:55 +0200

    move commands away from repl.js

---
+++ utils/commands.js
@@ -0,0 +1,111 @@
+const _ = require('lodash')
+const moment = require('moment')
+
+const User = require('../models/User')
+
+const help_text = `
+available commands:
+	users
+		add login, password, [prefix]
+		remove login
+		approve login
+		disapprove login
+		info login
+		pending 
+`
+
+module.exports = (cb) => { 
+	return {
+		help () {
+			return cb(help_text)
+		},
+		users: {
+			pending_cache: [],
+			add (login, password, prefix) {
+				let user = new User(login)
+				user.prefix = prefix || ''
+				if (user.register(password)) {
+					cb('user ' + login + ' added successfully')
+					return true
+				}
+				else {
+					cb('failed to add user')
+					return false
+				}
+			},
+
+			approve (...args) {
+				if (args.length === 0) return false
+				if (typeof args[0] === 'string') {
+					let user = new User(token)
+					if (user.exists) {
+						user.approved = true
+						user.save()
+						cb('user ' + token + ' approved successfully')
+						return true
+					} else {
+						cb('failed to approve user')
+						return false
+					}
+				} else if (typeof args[0] === 'number') {
+					_.each(args, (t) => {
+						let user = new User(this.pending_cache[t])
+						user.approved = true
+						user.save()
+					})
+					cb('users approved successfully')
+					return true
+				} else {
+					return false
+				}
+			},
+
+			disapprove (login) {
+				let user = new User(login)
+				if (user.exists) {
+					user.approved = false
+					cb('user ' + login + ' disapproved successfully')
+					return true
+				} else {
+					cb('failed to disapprove user')
+					return false
+				}
+			},
+
+			info (login) {
+				let user = new User(login)
+				if (user.exists) {
+					cb(user)
+					return true
+				} else return false
+			},
+
+			list () {
+				cb(User.getAllLogins().join(', '))
+				return true
+			},
+
+			pending () {
+				let i = 0
+				this.pending_cache = []
+				User.getPending().forEach((u) => {
+					this.pending_cache[i] = u.login
+					cb('[' + i++ + '] // ' + 
+						u.login + ' // ' + new moment(u.date_created).calendar())
+				})
+
+				if (!i) cb('nobody awaiting approval')
+				
+				return true
+			},
+
+			remove (login) {
+				let user = new User(login)
+				if (user.remove()) {
+					cb('user ' + login + ' removed successfully')
+					return true
+				} else return false
+			}
+		}
+	}
+}
--- utils/repl.js
+++ utils/repl.js
@@ -1,119 +1,14 @@
 const _ = require('lodash')
-const moment = require('moment')
-
-const User = require('../models/User')
 
 const r = require('repl')
 
-let ctx = {
-	help () {
-		console.log(help_text)
-	},
-	users: {
-		pending_cache: [],
-		add (login, password, prefix) {
-			let user = new User(login)
-			user.prefix = prefix || ''
-			if (user.register(password)) {
-				console.log('user ' + login + ' added successfully')
-				return true
-			}
-			else {
-				console.log('failed adding user')
-				return false
-			}
-		},
-
-		approve (...args) {
-			if (args.length === 0) return false
-			if (typeof args[0] === 'string') {
-				let user = new User(token)
-				if (user.exists) {
-					user.approved = true
-					console.log('user ' + token + ' approved successfully')
-					return true
-				} else {
-					console.log('failed to approve user')
-					return false
-				}
-			} else if (typeof args[0] === 'number') {
-				_.each(args, (t) => {
-					let user = new User(this.pending_cache[t])
-					user.approved = true
-					user.save()
-				})
-				console.log('users approved successfully')
-				return true
-			} else {
-				return false
-			}
-		},
-
-		disapprove (login) {
-			let user = new User(login)
-			if (user.exists) {
-				user.approved = false
-				console.log('user ' + login + ' disapproved successfully')
-				return true
-			} else {
-				console.log('failed to disapprove user')
-				return false
-			}
-		},
-
-		info (login) {
-			let user = new User(login)
-			if (user.exists) {
-				console.log(user)
-				return true
-			} else return false
-		},
-
-		list () {
-			console.log(User.getAllLogins().join(', '))
-			return true
-		},
-
-		pending () {
-			let i = 0
-			this.pending_cache = []
-			User.getPending().forEach((u) => {
-				this.pending_cache[i] = u.login
-				console.log('[' + i++ + '] // ' + 
-					u.login + ' // ' + new moment(u.date_created).calendar())
-			})
-
-			if (!i) console.log('nobody awaiting approval')
-			
-			return true
-		},
-
-		remove (login) {
-			let user = new User(login)
-			if (user.remove()) {
-				console.log('user ' + login + ' removed successfully')
-				return true
-			} else return false
-		}
-	}
-}
+let ctx = require('./commands')(console.log)
 
 let options = {
 	prompt: 'rooftop> ',
 	useGlobal: true,
 	context: ctx
 }
-
-const help_text = `
-available commands:
-	users
-		add login, password, [prefix]
-		remove login
-		approve login
-		disapprove login
-		info login
-		pending 
-	`
 
 module.exports.start = () => {
 	const repl = r.start(options)
